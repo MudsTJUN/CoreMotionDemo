2265e72bfbea60930321c5b42d9cf138792aba82
MudsTJUN <88066889+MudsTJUN@users.noreply.github.com>
2023-03-21 00:12:40 +0900

    Add files via upload

---
+++ CoreMLDDemo/CoreMLDDemo/CoreMLDDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreMLDDemoApp.swift
+//  CoreMLDDemo
+//
+//  Created by 塚本隼 on 2023/03/20.
+//
+
+import SwiftUI
+
+@main
+struct CoreMLDDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
